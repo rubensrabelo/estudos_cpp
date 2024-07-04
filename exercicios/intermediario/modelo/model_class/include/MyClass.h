0cbecc24d16a0f018e032ab8ac2a98e6a27dec0e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-04 09:50:50 -0300

    Creating model class

---
+++ exercicios/intermediario/modelo/model_class/include/MyClass.h
@@ -0,0 +1,10 @@
+// my_class.h
+
+#pragma once // Evita múltiplas inclusões acidentais
+
+namespace N {
+    class MyClass {
+    public:
+        void doSomething(); // Declaração de uma função de membro
+    };
+}
---
+++ exercicios/intermediario/modelo/model_class/src/MyClass.cpp
@@ -0,0 +1,9 @@
+// my_class.cpp
+
+#include "MyClass.h" // Inclui o arquivo de cabeçalho
+
+namespace N {
+    void MyClass::doSomething() {
+        // Implementação da função de membro
+    }
+}
