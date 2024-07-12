6282ea9df61762810b8b24b0acee13c38bd87ae3
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-12 10:13:17 -0300

    Rectangle area

--- exercicios/intermediario/classe/ex005/main.cpp
+++ exercicios/intermediario/classe/ex005/main.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
+#include "./include/Shape.h"
 
 using namespace std;
+using namespace Shape;
 
 int main(){
+    Rectangle rectangle(2, 2);
+
+    cout << "The area is " << rectangle.area() << endl;
+
+    system("pause");
+
     return 0;
 }
