ca7b3c88aa713601f39de6afaa2e3d4fd8ea8af4
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-23 11:34:42 -0300

    Rectangle Program

--- disciplina_ed/struct/Rectangle/main.cpp
+++ disciplina_ed/struct/Rectangle/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
@@ -15,9 +16,39 @@ struct Rectangle {
         this->sideSupLeft = point1;
         this->sideInfRight = point2;
     }
+
+    double length() {
+        return fabs(this->sideSupLeft.x - this->sideInfRight.x);
+    }
+
+    double width() {
+        return fabs(this->sideSupLeft.y - this->sideInfRight.y);
+    }
+
+    double diagonal() {
+        return sqrt(pow(this->sideSupLeft.x - this->sideInfRight.x, 2) + pow(this->sideSupLeft.y - this->sideInfRight.y, 2)); 
+    }
+
+    double area() {
+        return this->length() * this->width();
+    }
+
+    double perimeter() {
+        return 2*(this->length() + this->width());
+    }
 };
 
 int main(){
+    Point p1 = {0, 10};
+    Point p2 = {5, 0};
 
+    Rectangle rect(p1, p2);
+
+    cout << "Length: " << rect.length() << endl;
+    cout << "Width: " << rect.width() << endl;
+    cout << "Diagonal: " << rect.diagonal() << endl;
+    cout << "Area: " << rect.area() << endl;
+    cout << "Perimeter: " << rect.perimeter() << endl;
+    
     return 0;
 }
